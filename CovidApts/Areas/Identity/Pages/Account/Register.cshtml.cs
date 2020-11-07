eb70ba8cfc87a5986c32da02784a40e2cceb38bc
Idan <killingfactoryidan@gmail.com>
2020-11-07 20:04:30 +0200

    added some views and stuff

---
+++ CovidApts/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using CovidApts.Models;
+
+namespace CovidApts.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RegisterModel(
+            UserManager<User> userManager,
+            SignInManager<User> signInManager,
+            ILogger<RegisterModel> logger,
+            IEmailSender emailSender,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
+            _roleManager = roleManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "Role")]
+            public string SelectedRole { get; set; }
+
+            [Required]
+            [DataType(DataType.PhoneNumber)]
+            [Display(Name = "Phone")]
+            [Phone]
+            public string Phone { get; set; }
+
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "Address")]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            public string Address { get; set; }
+
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "FullName")]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            public string FullName { get; set; }
+        }
+        
+        public void OnGet(string returnUrl = null)
+        {			
+            var roles = from eUserRoles usrl in Enum.GetValues(typeof(eUserRoles))
+                           select new { Value = usrl.ToString(), Text = usrl.ToString() };
+            ViewData["Roles"] = new SelectList(roles, "Value", "Text");
+            ReturnUrl = returnUrl;
+			
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var user = new User {
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    FullName = Input.FullName,
+                    PhoneNumber =  Input.Phone,
+                    Address = Input.Address
+                };
+                var result = await _userManager.CreateAsync(user, Input.Password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { userId = user.Id, code = code },
+                        protocol: Request.Scheme);
+
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                    // Confirming the mail.
+                    // We cant really confirm the email address until we get the application on azure.
+                    result = _userManager.ConfirmEmailAsync(user, code).Result;
+
+                    // Continues only if the mail was confirmed successfully
+                    if (result.Succeeded)
+                    {
+                        // Checking if the selected role is available
+                        if (!_roleManager.RoleExistsAsync(Input.SelectedRole).Result)
+                            Input.SelectedRole = eUserRoles.Guide.ToString();
+
+                        // Adding the role to the new user
+                        result = _userManager.AddToRoleAsync(user, Input.SelectedRole).Result;
+
+                        // Continues only if the role was added successfully
+                        if (result.Succeeded)
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                            return LocalRedirect(returnUrl);
+                        }
+                    }
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            var roles = from eUserRoles usrl in Enum.GetValues(typeof(eUserRoles))
+                select new { Value = usrl.ToString(), Text = usrl.ToString() };
+            ViewData["Roles"] = new SelectList(roles, "Value", "Text");
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+    }
+}
---
+++ CovidApts/Data/CovidAptsDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CovidApts.Models;
+
+namespace CovidApts.Data
+{
+    public class CovidAptsDbContext : IdentityDbContext
+    {
+        public DbSet<Company> Company { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Apartment> Apartment { get; set; }
+
+
+        public CovidAptsDbContext(DbContextOptions<CovidAptsDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Company>()
+                .HasOne(mal => mal.CurrentApartment)
+                .WithMany(apt => apt.Companies)
+                .IsRequired();
+        }
+    }
+}
