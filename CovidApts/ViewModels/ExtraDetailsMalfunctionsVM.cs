b6b8d95f19183325f94966dfddd861951c9cf2a9
Idan <killingfactoryidan@gmail.com>
2020-11-07 20:13:06 +0200

    Added a view model for statistics

---
+++ CovidApts/ViewModels/ExtraDetailsMalfunctionsVM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using CovidApts.Models;
+
+namespace CovidApts.ViewModels
+{
+    public class ExtraDetailsCompaniesVM
+    {
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Display(Name = "Status")]
+        public string Status { get; set; }
+
+        [Display(Name = "Content")]
+        public string Content { get; set; }
+
+        [Display(Name = "Creation date")]
+        public DateTime CreationDate { get; set; }
+
+        [Display(Name = "Modified date")]
+        public DateTime ModifiedDate { get; set; }
+
+        [Display(Name = "Appartment address")]
+        public String AppartmentAddress { get; set; }
+
+        [Display(Name = "User name")]
+        public String UserName { get; set; }
+    }
+}
