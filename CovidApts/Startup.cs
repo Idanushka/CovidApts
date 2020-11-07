3c7f38a4954b81b904e780760861e4cd7379e360
Idan <killingfactoryidan@gmail.com>
2020-11-07 19:54:04 +0200

    Started working - template

---
+++ CovidApts/Startup.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CovidApts.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using CovidApts.Models;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using CovidApts.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
+namespace CovidApts
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;                
+            });
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = false;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;
+            });
+
+
+            services.AddDbContext<CovidAptsDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddDefaultUI()
+                .AddEntityFrameworkStores<CovidAptsDbContext>()
+                .AddDefaultTokenProviders();
+
+            //services.AddAuthentication().AddFacebook(facebookOptions =>
+            //{
+            //    facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
+            //    facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+            //});
+            
+            services.AddTransient<IEmailSender, EmailSender>(i =>
+                new EmailSender(
+                    Configuration["EmailSender:Host"],
+                    Configuration.GetValue<int>("EmailSender:Port"),
+                    Configuration.GetValue<bool>("EmailSender:EnableSSL"),
+                    Configuration["EmailSender:UserName"],
+                    Configuration["EmailSender:Password"]));
+            
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider service)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Apartments/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Apartments}/{action=Index}/{id?}");
+            });
+
+            CreateUserRoles(service);
+        }
+
+        private void CreateUserRoles(IServiceProvider serviceProvider)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            IdentityResult roleResult;
+
+            foreach (var role in Enum.GetValues(typeof(eUserRoles)))
+            {
+                var currentRole = role.ToString();
+
+                //Adding all Roles if the don't exist in DB
+                var roleCheck = RoleManager.RoleExistsAsync(currentRole).Result;
+                if (!roleCheck)
+                {
+                    //create the roles and seed them to the database  
+                    roleResult = RoleManager.CreateAsync(new IdentityRole(currentRole)).Result;
+                }
+            }
+        }
+    }
+}
---
+++ CovidApts/wwwroot/js/site.js
@@ -0,0 +1,86 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function initMap() {
+    getWeather();
+    var latitudeElement = document.getElementById("Latitude");
+    var longitudeElement = document.getElementById("Longitude");
+    var mapElement = document.getElementById('map');
+
+    // If there is no map element quit the init map function
+    if (mapElement == null) {
+        return;
+    }
+
+    // If we don't have a Latitude and Longitude elements sets the map to default location
+    if (latitudeElement == null || longitudeElement == null) {
+        var uluru = { lat: 31.771959, lng: 35.217018 };
+        var mapZoom = 9;
+    }
+    else {
+        var uluru = {
+            lat: Number.parseFloat(latitudeElement.value),
+            lng: Number.parseFloat(longitudeElement.value)
+        };
+        var mapZoom = 14;
+    }
+
+    // The map, centered at Uluru
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: mapZoom,
+        center: uluru
+    });
+
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({ position: uluru, map: map });
+}
+
+function addWeather(day, condition) {
+   $("#weather").append('<li>' + "<div><b>" + day + "</div></b>" + "<p>" + "</p>"+'</li>');
+}
+
+function convertToCelsius(temp) {
+    return Math.round(5 * (temp - 32) / 9);
+}
+
+function getWeather() {
+    var latitudeElement = document.getElementById("Latitude");
+    var longitudeElement = document.getElementById("Longitude");
+
+    var DEG = "Celsius";
+    var lat = 31.771959;
+    var lng = 35.217018;
+
+    //if (latitudeElement != null && longitudeElement != null) {
+    //    lat = latitudeElement.value;
+    //    lng = longitudeElement.value;
+    //}
+    
+    var url = 'https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lng +'&appid=d4d7a1a00144183b60e1b8f777d932ed&units=metric'
+
+    // Make a weather API request (it is JSONP, so CORS is not an issue):
+    $.ajax({
+        url: url,
+        type: "GET",
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        success: function (r) {
+
+            console.log(r);
+     
+
+                // Create the weather items in the #scroller UL
+
+                var item = r.main;
+                addWeather("Average temperature today is:  " + item.temp + ' ° ' + DEG);          
+            
+        }
+    });
+}
