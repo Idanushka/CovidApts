9b2c9c5265e6e2b7442b7b36374d54a342e35d0c
Omri Siniver <omri.siniver1@gmail.com>
2020-11-14 13:54:16 +0200

    Add Company and User models

---
+++ CovidApts/Models/User.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidApts.Models
+{
+    public class User : IdentityUser
+    {
+        #region Properties
+
+        public string UserId { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(25)]
+        [DisplayName("Full Name")]
+        public string FullName { get; set; }
+        
+        public string FacebookId { get; set; }
+        
+        [Required]
+        [MinLength(4)]
+        [MaxLength(25)]
+        public string Address { get; set; }
+
+        public ICollection<Company> Companies { get; set; }
+
+        #endregion
+    }
+
+    public enum eUserRoles
+    {
+        Guide,
+        Janitor,
+        SocialWorker,
+        Admin
+    }
+}
