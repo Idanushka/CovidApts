1fbd2069df91c47b42680d046ff3a230cef0c302
yuval sages <yuvalsages@gmail.com>
2020-11-15 19:08:02 +0200

    Added the Apartments model and views

---
+++ CovidApts/Models/Apartment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidApts.Models
+{
+    public class Apartment
+    {
+        #region Properties
+
+        public int ApartmentId { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(50)]
+        public string Address { get; set; }
+
+        public double Longitude { get; set; }
+
+        public double Latitude { get; set; }
+
+        public string Photo { get; set; }
+
+        [Range(1, 30)]
+        [DisplayName("Rooms Number")]
+        public int RoomsNumber { get; set; }
+
+        public ICollection<Company> Companies { get; set; }
+
+        #endregion
+    }
+}
